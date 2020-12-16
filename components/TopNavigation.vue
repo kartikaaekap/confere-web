fda26984f72126ce3252d0ea3d338a56d62ca5a2
kartikaaekap <kartikaaekap@gmail.com>
2020-12-16 09:51:20 +0700

    mobile responsive

--- components/TopNavigation.vue
+++ components/TopNavigation.vue
@@ -3,7 +3,28 @@
     <b-navbar fixed="top" class="navbar" :class="{ 'navbar--shadow': scrollPosition }">
       <b-container class="px-0 px-sm-3">
         <b-col cols="4" class="d-block d-md-none">
-          <base-button variant="secondary" size="small" icon="list" icon-only @click="toggleSidebar" />
+          <dropdown-menu v-model="isDropdownOpenMenu" left hover>
+            <base-button
+              variant="secondary"
+              icon="list"
+              size="small"
+              class="ml-3"
+              icon-only
+            />
+            <div slot="dropdown">
+              <template>
+                <base-button v-if="this.$store.state.user.roles === 'student'" variant="text" to="/dashboard/home">
+                  Home
+                </base-button>
+                <base-button v-else variant="text" to="/teacher/home">
+                  Home
+                </base-button>
+                <base-button v-if="this.$store.state.user.roles === 'student'" variant="text" to="/dashboard/classlist">
+                  List Class
+                </base-button>
+              </template>
+            </div>
+          </dropdown-menu>
         </b-col>
         <b-col cols="4" md="2" class="d-flex px-0 px-md-3">
           <b-navbar-brand href="#" class="d-flex px-0 px-md-3">
@@ -54,68 +75,6 @@
         </b-collapse>
       </b-container>
     </b-navbar>
-    <aside v-if="$route.path === '/'" class="sidebar" :class="{ 'sidebar--open': isSidebarOpen }">
-      <b-container class="px-0">
-        <b-navbar class="sidebar__header px-0">
-          <b-col cols="4" class="d-block d-md-none">
-            <base-button variant="secondary" size="small" icon="x" icon-only @click="toggleSidebar" />
-          </b-col>
-          <b-col cols="4" md="2" class="d-flex px-0 px-md-3">
-            <a class="mx-auto mx-md-0">
-              <b-img src="~/assets/img/logo-confere.png" fluid />
-            </a>
-          </b-col>
-          <b-col cols="4" class="text-right">
-            <dropdown-menu v-model="isDropdownOpen" right hover>
-              <base-button
-                id="user"
-                variant="secondary"
-                icon="person"
-                size="small"
-                class="ml-3"
-                icon-only
-                is-circle
-              />
-              <div slot="dropdown">
-                <template v-if="!isAdmin">
-                  <p class="profil__name pl-2 py-2 mb-0">
-                    Kartika Eka Putri
-                  </p>
-                  <p class="profil__description pl-2 pb-2 mt-0">
-                    kartikaaekap@gmail.com
-                  </p>
-                </template>
-                <b-link class="dropdown__item p-2" to="/signout">
-                  Sign Out
-                </b-link>
-              </div>
-            </dropdown-menu>
-          </b-col>
-        </b-navbar>
-        <div class="sidebar__menu px-3">
-          <b-nav vertical>
-            <div class="sidebar__menu--top">
-              <template
-                v-if="$route.path === '/' || $route.path === '/pricing' || $route.path === '/features' || $route.path.includes('/help')
-                  || $route.path === '/aboutUs' || $route.path === '/terms'"
-              >
-                <base-button variant="text" is-full to="/pricing">
-                  Home
-                </base-button>
-                <base-button variant="text" is-full to="/help">
-                  Class
-                </base-button>
-              </template>
-              <div v-else class="sidebar__menu--bottom">
-                <base-button variant="secondary" to="/signout" is-full>
-                  Sign Out
-                </base-button>
-              </div>
-            </div>
-          </b-nav>
-        </div>
-      </b-container>
-    </aside>
   </b-container>
 </template>
 
@@ -135,14 +94,10 @@ export default {
     return {
       scrollPosition: null,
       isDropdownOpen: false,
+      isDropdownOpenMenu: false,
       openModal: ''
     }
   },
-  // computed: {
-  //   isStudent () {
-  //     return this.state.user.roles === 'student'
-  //   }
-  // },
   mounted () {
     window.addEventListener('scroll', this.updateScroll)
   },
@@ -155,36 +110,6 @@ export default {
       this.scrollPosition = window.scrollY
     }
   }
-  // computed: {
-  //   ...mapState(['isSidebarOpen']),
-  //   listMenu () {
-  //     return this.user ? [{ name: 'Profile', path: '/profile' }] : [{ name: 'About us', path: '#' }, { name: 'Features', path: '#' }, { name: 'Pricing', path: '#' }]
-  //   },
-  //   isAdmin () {
-  //     return this.user && this.user.isAdmin
-  //   },
-  //   credits () {
-  //     return this.user ? (this.user.credits ? this.user.credits.amount / this.qoinBasePrice : 0) : null
-  //   },
-  //   checkCompany () {
-  //     return this.user && this.user.company.length
-  //   },
-  //   checkReceipt () {
-  //     return this.$store.state.haveReceipt
-  //   },
-  //   variables () {
-  //     return this.$store.state.variables
-  //   }
-  // },
-  // mounted () {
-  //   if (this.user && !this.isAdmin) {
-  //     this.$store.dispatch('getVariables').then(variables => this.$store.commit('updateVariables', variables))
-  //   }
-  //   window.addEventListener('scroll', this.updateScroll)
-  // },
-  // destroy () {
-  //   window.removeEventListener('scroll', this.updateScroll)
-  // }
 }
 </script>
 
